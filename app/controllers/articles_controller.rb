202a6f2bc163ad2ce10bbbdfcde877d1dc0f146b
Tomasz Klepacki <t.klepacki@gmail.com>
2019-01-22 17:19:49 +0100

    Added edit and delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,19 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+      @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+         if @article.update(article_params)
+            redirect_to @article
+          else
+            render 'edit'
+          end
+    end
 
     def create
     @article = Article.new(article_params)
@@ -21,6 +34,13 @@ class ArticlesController < ApplicationController
     else
         render 'new'
       end
+    end
+
+    def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+
+      redirect_to articles_path
     end
 
     private
